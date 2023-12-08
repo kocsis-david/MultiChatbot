5e091b9d41edeb19ea6322b38e98cd66b41e22fd
Kocsis Dávid <kocsis.david@edu.bme.hu>
2023-12-08 16:55:38 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+from dotenv import load_dotenv
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain import HuggingFaceHub
+#from langchain.embeddings import cohere
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+#from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+#HUGGINGFACEHUB_API_TOKEN=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
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Ask your PDF")
+
+    # upload file
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+
+    # extract the text
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        # split into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=400,
+            chunk_overlap=50,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text)
+
+        # create embeddings
+        embeddings =HuggingFaceInstructEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-mpnet-base-v2")
+        knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+        # show user input
+        user_question = st.text_input("Ask a question about your PDF:")
+        if user_question:
+            docs = knowledge_base.similarity_search(user_question)
+
+            llm = HuggingFaceHub(repo_id="google/flan-t5-xxl")
+            chain = load_qa_chain(llm, chain_type="stuff")
+            with get_openai_callback() as cb:
+                response = chain.run(input_documents=docs, question=user_question)
+                print(cb)
+
+            st.write(response)
+
+
+if __name__ == '__main__':
+    main()
