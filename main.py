970333c8b0709f540c77ee7f57e42fd6ee686fae
Kocsis Dávid <120012184+kocsis-david@users.noreply.github.com>
2024-05-02 16:36:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 #from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
-#HUGGINGFACEHUB_API_TOKEN=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
+
 
 def main():
     load_dotenv()
